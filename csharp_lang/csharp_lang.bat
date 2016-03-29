24db160785dd868ec6fd7bd2902025fa26d36ac0
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-03-29 14:22:34 +0200

    2016-03-29 csharp_lang.bat 2

---
+++ csharp_lang/csharp_lang.bat
@@ -0,0 +1,21 @@
+@echo off
+
+rem A batch file for csharp_lang.exe
+rem which captures the app's return value.
+
+csharp_lang
+
+@if "%ERRORLEVEL%" == "0" goto success
+
+:fail
+echo This application has failed!
+echo return value = %ERRORLEVEL%
+goto end
+
+:success
+echo This application has succeeded!
+echo return value = %ERRORLEVEL%
+goto end
+
+:end
+echo All done.
